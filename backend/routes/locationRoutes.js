b40a3446feb444c95b4783ceecf154c5d1889362
Berylmys <73904450+berylmys@users.noreply.github.com>
2024-12-14 17:38:21 +0800

    14-2

--- backend/routes/locationRoutes.js
+++ backend/routes/locationRoutes.js
@@ -8,10 +8,12 @@ const {
 } = require('../controllers/locationController');
 const authenticate = require('../middleware/authenticate');
 const authorizeAdmin = require('../middleware/authorizeAdmin');
+const { searchLocations } = require('../controllers/locationController');
 
 router.get('/', authenticate, getAllLocations);
 router.post('/', authenticate, authorizeAdmin, createLocation);
 router.get('/tenrandom', authenticate, getTenRandomLocations);
+router.get('/search', authenticate, searchLocations);
 // Additional routes for update, delete
 
 module.exports = router;
